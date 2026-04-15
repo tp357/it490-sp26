4fea05d70b94584edba6f3a4f9984501e133d7ce
Phoenix <djs234@njit.edu>
2026-04-15 12:32:11 -0400

    did some work on making adding movies work along with checking movies and adding phone to registration

--- sqlwork/MovieDBServer.php
+++ sqlwork/MovieDBServer.php
@@ -14,13 +14,45 @@ function getMovie(){
 
 }
 
+function addMovie($title, $year, $rating, $released, $runtime, $genre, $director, $writer, $actors, $plot, $language, $country, $poster){
+
+ $mydb = new mysqli('127.0.0.1','testuser','testpassword','490db');
+        if ($mydb->errno != 0)
+{
+        echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+        return false;
+        }
+	$checkq="SELECT * FROM movies WHERE TITLE='$title'";
+	$response=$mydb->query($checkq);
+	if(mysqli_num_rows($response)!=0){
+		$addquery="INSERT INTO movies VALUES ('$title', '$year', '$rating', $released', $runtime', '$genre', '$director', '$writer', '$actors', '$plot', '$language', '$country', '$poster')";
+		$mydb->query($addquery);
+		return true;
+	}
+
+}
+function movieCheck($title){
+ $mydb = new mysqli('127.0.0.1','testuser','testpassword','490db');
+        if ($mydb->errno != 0)
+{
+        echo "failed to connect to database: ". $mydb->error . PHP_EOL;
+        return false;
+        }
+	$checkquery="SELECT * FROM movies WHERE TITLE='$title'";
+	$response=$mydb->query($checkquery);
+	if($mysqli_num_rows($response)=0){
+		return true;
+	} else {
+		return false;
+	}
+}
 
 
 function requestProcessor($request)
 {
         $returnstatus = false;
         $message = array();
-        $sessionID=NULL;
+	$exists=NULL;
   echo "received request".PHP_EOL;
   var_dump($request);
   if(!isset($request['type']))
@@ -29,11 +61,19 @@ function requestProcessor($request)
     return "ERROR: unsupported message type";
   }
   switch ($request['type'])
-	{
+  {
+ 		 case "add_movie":
+	  		 add_Movie($request['title'], $request ['year'], $request['rating'], $request['released'], $request['runtime'], $request['genre'], $request['director'], $request['writer'], $request['actors'], $request['plot'], $request['language'], $request['country'], $request['poster']);
+			 echo "debugging add movie\n";
+			 break; 
  		 case "get_one_movie":
 			echo "hi this is debugging get :D";
 			break;
-
+		case "if_movie_exists";
+			$returnstatus=movieCheck($request['title'];	
+			$exists=$returnstatus
+			echo "hi this is debugging movie check\n";		
+			break;
 		case "get_movies":
 			echo "hi this is debugging get movies";
 			break;
@@ -42,10 +82,13 @@ function requestProcessor($request)
 			echo "hi this is debugging send :D";
 			break;
   	}
-  if($returnstatus){
+  if($returnstatus && $exists){
+        $message = array("status" => 'success', 'message'=>"Server received request and processed", 'exists'=>"$exists");
+  } elseif {
         $message = array("status" => 'success', 'message'=>"Server received request and processed");
-  } else {
-        $message = array("status"=>"This shit ain't work");
+	}else {
+	$exists=false;
+        $message = array("exists"=>"$exists");
   }
 echo $message;
   return $message;
